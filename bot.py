4100d37d8111c820dd6ac6540b8dfb21e8f6ee76
DanyB0 <daniele.beltrami13@gmail.com>
2022-05-24 14:59:33 -0700

    Fix messages formatting

--- bot.py
+++ bot.py
@@ -29,11 +29,14 @@ os.chdir(utils.BASE_DIR)
 intents = Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="§", intents=intents)
 
 # messages list
 with open("frasi.txt", "r", encoding="utf8") as fl:
-    frasi = fl.readlines()
+    frasi_list = fl.readlines()
+
+# emoji
+emoji_list = [":fire:", ":books:", ":recycle:"]
 
 scheduler = AsyncIOScheduler()
 scheduler.configure(timezone="utc")
@@ -45,18 +48,28 @@ async def on_ready():
     # change the ID to your channel ID
     # channel = bot.get_channel(CHANNEL_ID)
     channel = bot.get_channel(951156001434914866)
-    print(channel)
+    print(f"canale motivazionale: {channel}")
     utils.write_logs("Start", "Bot started")
 
     async def send():
-        # get a random number that will be the index of the message in the list
-        i = random.randint(0, len(frasi) - 1)
-        await channel.send(f"Stay motivated!:\n\n***{frasi[i]}***")
+        # get a random phrase and a random emoji
+        frase = random.choices(frasi_list)[0]
+        emoji = random.choices(emoji_list)[0]
+        # the message that will be sent
+        message = f"""
+                    -------------------------
+{emoji}   ||@everyone||   {emoji}
+-------------------------
+**FRASE MOTIVAZIONALE DEL GIORNO**
+{frase}
+"""
+
+        await channel.send(message)
         print("Messaggio inviato :)")
         utils.write_logs("Message", "Message sent")
 
     # calls the function everyday
-    scheduler.add_job(send, "interval", seconds=86400)
+    scheduler.add_job(send, "interval", seconds=3)
 
 
 # run the bot
